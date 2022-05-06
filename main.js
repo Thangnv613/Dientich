6006e7b0ed27c56409880f0380d0039b7f215937
Thang cg <duongsag.hp11@gmail.com>
2022-05-06 11:17:27 +0700

    Up

---
+++ main.js
@@ -0,0 +1,35 @@
+//Bài 1: Viết chương trình nhập điểm của một sinh viên cho các môn: Vật lý, Hóa học, và Sinh học.
+// Sau đó hiển thị điểm trung bình và tổng của những điểm này.
+
+
+// let VL=prompt("Moi nhap điểm vật lý");
+//     VL=parseFloat(VL)
+// let HH=prompt("Moi nhap điểm hóa học");
+//     HH=parseFloat(HH)
+// let SH=prompt("Moi nhap điểm sinh học");
+//     SH=parseFloat(SH)
+// DTB=(VL+HH+SH)/3;
+// Tong=VL+HH+SH;
+// document.write("Điểm vật lý: " + VL + "đ")
+// document.write("<br>Điểm hóa học: " + HH + "đ")
+// document.write("<br>Điểm sinh học: " + SH + "đ")
+// document.write("<br>Điểm Trung bình môn: " +"<br>" + DTB + "đ")
+// document.write("<br>Tổng điểm ba môn: " +"<br>" + Tong + "đ")
+
+
+//Bài 2:  Viết chương trình nhập một giá trị là độ 0C (Celsius) và chuyển nó sang độ 0F (Fahrenheit).
+// [Hướng dẫn: C/5 = (F-32)/9]
+
+// let c=prompt("Mời nhập độ C")
+// F=((9*c)/5)+32
+// document.write( c +" độ C" + " tương đương là: " + F + " độ F")
+
+//Bài 3: Viết chương trình tính diện tích hình tròn
+
+let r=prompt("Mời nhập bán kinh");
+    area=3.14*r**2;
+    peri=2*3.14*r;
+document.write("Diện tích của anh Chính là: " + area);
+document.write("<br>Chu vi của anh Chính là: " + peri)
+
+
